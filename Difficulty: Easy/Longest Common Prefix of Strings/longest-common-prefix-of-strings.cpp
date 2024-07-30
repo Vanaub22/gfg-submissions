6af6c4b2f3a21e8542da9905684890fefabfd4ef
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-07-31 01:17:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Longest Common Prefix of Strings/longest-common-prefix-of-strings.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+
+    string longestCommonPrefix(vector<string> arr) {
+        string x=arr[0];
+        int n=x.size();
+        for(string& word:arr) for(int i=0;i<n;i++) if(x[i]!=word[i]) x=x.substr(0,i);
+        return x.empty()?"-1":x;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<string> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        string number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.longestCommonPrefix(arr) << endl;
+    }
+}
+
+// } Driver Code Ends
