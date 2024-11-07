91c2f13ae0e198cd5f051749d8a05717d84245a7
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-11-07 09:27:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Split array in three equal sum subarrays/split-array-in-three-equal-sum-subarrays.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+//  Class Solution to contain the method for solving the problem.
+class Solution {
+  public:
+    // Function to determine if array arr can be split into three equal sum sets.
+    vector<int> findSplit(vector<int>& arr) {
+        int tsum=accumulate(arr.begin(),arr.end(),0),rsum=0;
+        if(tsum%3) return {-1,-1};
+        vector<int> ans;
+        for(int i=0;i<arr.size();i++) {
+            rsum+=arr[i];
+            if(rsum==tsum/3) {
+                ans.emplace_back(rsum);
+                rsum=0;
+            }
+            else if(rsum>tsum/3) return {-1,-1};
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_cases;
+    cin >> test_cases;
+    cin.ignore();
+    while (test_cases--) {
+        string input;
+        vector<int> arr;
+
+        // Read the array from input line
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        // Solution instance to invoke the function
+        Solution ob;
+        vector<int> result = ob.findSplit(arr);
+
+        // Output result
+        if (result[0] == -1 && result[1] == -1) {
+            cout << "false" << endl;
+        } else {
+            cout << "true" << endl;
+        }
+    }
+    return 0;
+}
+
+// } Driver Code Ends
